60a1a4e050d50eaf4c04ae491c8ec058ea523475
zxcodxe <saifilaibaa33@gmail.com>
2026-08-13 00:19:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,28 @@ from logging.handlers import RotatingFileHandler
 import os
 
 LOG_FILE_NAME = "bot.log"
-PORT = os.environ.get('PORT', '8080')
-OWNER_ID = 
+PORT = os.environ.get("PORT", "8080")
+OWNER_ID = 7808531413
 MSG_EFFECT = 5046509860389126442
+
 
 def LOGGER(name: str, client_name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     formatter = logging.Formatter(
         f"[%(asctime)s - %(levelname)s] - {client_name} - %(name)s - %(message)s",
-        datefmt='%d-%b-%y %H:%M:%S'
+        datefmt="%d-%b-%y %H:%M:%S"
     )
-    file_handler = RotatingFileHandler(LOG_FILE_NAME, maxBytes=50_000_000, backupCount=10)
+
+    file_handler = RotatingFileHandler(
+        LOG_FILE_NAME,
+        maxBytes=50_000_000,
+        backupCount=10
+    )
     file_handler.setFormatter(formatter)
+
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
+
     logger.setLevel(logging.INFO)
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
